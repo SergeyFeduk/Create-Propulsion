16ee86eb21d670c583852af4241c928385fa7590
Sergey Feduk <saf207207@gmail.com>
2025-09-17 01:37:01 +0600

    Better hole model & thresholding Better invalidation-based balloon murder

--- src/main/java/com/deltasf/createpropulsion/balloons/hot_air/HotAirSolver.java
+++ src/main/java/com/deltasf/createpropulsion/balloons/hot_air/HotAirSolver.java
@@ -16,24 +16,19 @@ import net.minecraft.world.level.Level;
 //"Solver" word is a bit of an overkill, but it sounds cooler this way :D
 public class HotAirSolver {
     static final double surfaceAreaFactor = 6;
-    static final double epsilon = 1e-2;
+    static final double epsilon = 0.1;
+    static final double holeFactorExponent = 1.5;
+    static final double holeInvalidationThresholdPercent = 0.25;
+    static final double catastrophicLeakFactor = 1000.0;
 
     public static boolean tickBalloon(Level level, Balloon balloon, HaiGroup group, BalloonRegistry registry) {
-        double hotAirAmount = balloon.hotAir;
-        double hotAirChange = 0;
-
         if (balloon.isEmpty()) {
             return true; //Dead in a moment
         }
-        //Handle invalidation
-        if (balloon.isInvalid && hotAirAmount <= epsilon) {
-            //Recheck validity
-            balloon.isInvalid = !BalloonRegistryUtility.isBalloonValid(balloon, group);
-            if (balloon.isInvalid) {
-                //Bro did not survive this
-                return true;
-            }
-        }
+        
+        double hotAirAmount = balloon.hotAir;
+        double hotAirChange = 0;
+        
         //Hai injections
         for(UUID id : balloon.supportHais) {
             AbstractHotAirInjectorBlockEntity hai = registry.getInjector(level, id); 
@@ -45,16 +40,30 @@ public class HotAirSolver {
         double volume = balloon.getVolumeSize();
         double fullness = hotAirAmount / volume;
         double leakAdjustedFullness = Math.max(fullness, 0.1); //Use min here so leak is still significant for almost empty balloons
-        //Global surface leak
         double surfaceArea = surfaceAreaFactor * Math.pow(volume, 2.0/3.0);
-        hotAirChange -= PropulsionConfig.BALLOON_SURFACE_LEAK_FACTOR.get() * surfaceArea * leakAdjustedFullness;
-        //Hole leak
-        //TODO: Make dependency not linear. Smth like x^2 or worse so larger / more holes are significantly worse
-        hotAirChange -= balloon.holes.size() * PropulsionConfig.BALLOON_HOLE_LEAK_FACTOR.get() * leakAdjustedFullness; 
+        boolean isStructurallyFailed = balloon.holes.size() >= surfaceArea * holeInvalidationThresholdPercent;
+        double catastrophicFailureModifier = isStructurallyFailed ? catastrophicLeakFactor : 1.0;
 
+        //Global surface leak
+        hotAirChange -= PropulsionConfig.BALLOON_SURFACE_LEAK_FACTOR.get() * catastrophicFailureModifier * surfaceArea * leakAdjustedFullness;
+        //Hole leak
+        hotAirChange -= PropulsionConfig.BALLOON_HOLE_LEAK_FACTOR.get() * Math.pow(balloon.holes.size(), holeFactorExponent) * leakAdjustedFullness;
+        
         //Update hotAirAmount
         final double dt = 1 / 20.0; //For now a second will be the unit of time, may change
         balloon.hotAir = org.joml.Math.clamp(0, volume, hotAirAmount + hotAirChange * dt);
+
+        //Handle invalidation
+        boolean needsInvalidationCheck = balloon.isInvalid;
+        if (isStructurallyFailed) {
+            balloon.isInvalid = true;
+        } else if (needsInvalidationCheck) {
+            balloon.isInvalid = !BalloonRegistryUtility.isBalloonValid(balloon, group);
+        }
+
+        if (balloon.isInvalid && balloon.hotAir <= epsilon) {
+            return true;
+        }
 
         return false;
     }
