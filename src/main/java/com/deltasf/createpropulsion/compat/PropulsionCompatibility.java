68b913ee51e8ea1d6546ef178e897e9a3f7d7e12
Sergey Feduk <saf207207@gmail.com>
2025-09-26 10:18:12 +0600

    Removed shimmer compat checker

--- src/main/java/com/deltasf/createpropulsion/compat/PropulsionCompatibility.java
+++ src/main/java/com/deltasf/createpropulsion/compat/PropulsionCompatibility.java
@@ -6,5 +6,4 @@ public class PropulsionCompatibility {
     public static final boolean CBC_ACTIVE = ModList.get().isLoaded("createbigcannons");
     public static final boolean CC_ACTIVE = ModList.get().isLoaded("computercraft");
     public static final boolean JEI_ACTIVE = ModList.get().isLoaded("jei");
-    public static final boolean SHIMMER_ACTIVE = ModList.get().isLoaded("shimmer");
 }
